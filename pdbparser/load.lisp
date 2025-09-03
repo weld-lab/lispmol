ea68507d2df75cf332dba3245ba9f5924710b07b
atomicwelding <ctfweld@gmail.com>
2025-09-03 09:42:46 +0200

    API for loading pdb

--- pdbparser/load.lisp
+++ pdbparser/load.lisp
@@ -1,6 +1,17 @@
 (in-package #:lispmol.pdbparser)
 
 
-(defun load-pdb ()
-  "Not yet implemented"
+(defgeneric load-pdb (source)
+  (:documentation "Parse PDB ATOM/HETATM records from SOURCE and return a list of @atom."))
+
+
+(defmethod load-pdb ((source stream))
+  "not yet implemented"
   nil)
+
+(defmethod load-pdb ((source pathname))
+  (with-open-file (in source :direction :input)
+    (load-pdb in)))
+
+(defmethod load-pdb ((source string))
+  (load-pdb (pathname source)))
