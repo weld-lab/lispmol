5ba9e2b95566c814549349b2a51fe4c5c8de61be
atomicwelding <ctfweld@gmail.com>
2025-09-16 19:27:21 +0200

    make-_-from-record minimally implemented

--- preprocessor/pdb-topology.lisp
+++ preprocessor/pdb-topology.lisp
@@ -1,11 +1,26 @@
 (in-package #:lispmol.preprocessor)
 
+(defmethod make-molecule-from-record ((@p pdbparser:@pdb))
+  (let ((table (group-by-residue @p))
+	(mol (make-instance 'topology:molecule :molecule-name "empty")))
+    (maphash
+     #'(lambda (key bucket)
+	 (push (make-instance 'topology:residue
+			      :residue-id (third key)
+			      :residue-name (first key)
+			      :residue-atoms (loop for @a in bucket
+						   collect (make-atom-from-record @a)))
+	       (topology:molecule-residues mol)))
+     table)
+    mol))
 
-(defmethod make-molecule-from-record ((p pdbparser:@pdb))
-  (declare (ignore p))
-  (error "Not implemented yet"))
 
-
-(defmethod make-atom-from-record ((a pdbparser:@atom))
-  (declare (ignore a))
-  (error "Not implemented yet"))
+(defmethod make-atom-from-record ((@a pdbparser:@atom))
+  (make-instance 'topology:atom
+		 :atom-id (pdbparser:@atom-serial @a)
+		 :atom-name (pdbparser:@atom-name @a)
+		 :atom-element (pdbparser:@atom-element @a)
+		 :atom-x (pdbparser:@atom-x @a)
+		 :atom-y (pdbparser:@atom-y @a)
+		 :atom-z (pdbparser:@atom-z @a)
+		 :atom-residue-id (pdbparser:@atom-resseq @a)))
