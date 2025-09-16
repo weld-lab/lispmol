3d7bce1820d8e69a3aa63bbdc92219f9c8c439ec
atomicwelding <ctfweld@gmail.com>
2025-09-16 22:07:43 +0200

    more tests

--- tests/preprocessor.lisp
+++ tests/preprocessor.lisp
@@ -5,16 +5,33 @@
 
 (test
     group-by-residue-valid
-  
   (let* ((@p (pdbparser:load-pdb "lispmol/tests/6WSK.pdb"))
 	 (table (preprocessor:group-by-residue @p))
-	 (key '("GLY" #\A -165 #\ ))
+	 (key '("GLY" #\A -165 #\Space))
 	 (bucket (gethash key table)))
-    (is (= (length bucket) 7)))
-
-
+    (is (not (null bucket)))
+    (is (= (length bucket) 7))
+    (is (every (lambda (@a)
+                 (string= "GLY" (pdbparser:@atom-resname @a)))
+               bucket)))
+  
   (let* ((@p (pdbparser:load-pdb "lispmol/tests/2RH1.pdb"))
 	 (table (preprocessor:group-by-residue @p))
-	 (key '("ILE" #\A 72 #\ ))
+	 (key '("ILE" #\A 72 #\Space))
 	 (bucket (gethash key table)))
-    (is (= (length bucket) 8))))
+    (is (not (null bucket)))
+    (is (= (length bucket) 8))
+    (is (every (lambda (@a)
+                 (string= "ILE" (pdbparser:@atom-resname @a)))
+               bucket))))
+
+
+(test
+    molecule-from-record-valid
+  (let* ((@p (pdbparser:load-pdb "lispmol/tests/2RH1.pdb"))
+         (mol (preprocessor:make-molecule-from-record @p))
+         (natoms-in-pdb (length (pdbparser:@pdb-atoms @p)))
+         (natoms-in-mol (length
+			 (loop for r in (topology:molecule-residues mol)
+                               append (topology:residue-atoms r)))))
+    (is (= natoms-in-mol natoms-in-pdb))))
